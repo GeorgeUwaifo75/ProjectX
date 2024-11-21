3f107250bbb5355539a2b89655cef86eccca4766
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-21 15:54:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ import json
 from dotenv import load_dotenv
 
 
-# The API endpoint
-url =  "https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=10&api_key=f40243a801d1e370c14f7fa36a71cab5cce7c1c035e9551fc7a0592901f3ea53";
-
-# A GET request to the API
-response = requests.get(url)
-
-df = pd.read_json(url)
 
 def do_initApp():
   st.title("Short term Currency evaluation")
 
-def handle_userinput():
-  pass
+def handle_userinput(coin):
+  # The API endpoint
+  url =  "https://min-api.cryptocompare.com/data/v2/histohour?fsym={coin}&tsym=USD&limit=10&api_key=f40243a801d1e370c14f7fa36a71cab5cce7c1c035e9551fc7a0592901f3ea53";
+
+  # A GET request to the API
+  response = requests.get(url)
+
+  df = pd.read_json(url)
+
 
 def main():
   load_dotenv()
   st.set_page_config(page_title="Short term Currency evaluation", page_icon=":books:")
     
   do_initApp()
-  user_question = st.text_input("Ask a question about Crypto coin:")
+  user_question = st.text_input("Ask a question about Crypto any coin:")
 
   # Ask a question
   if user_question:
