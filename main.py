e2ca4c5663c6955c3519ad7c07d7869f5adce868
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-11-21 15:43:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 import json
 from dotenv import load_dotenv
 
-from datetime import datetime
 
 # The API endpoint
 url =  "https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=10&api_key=f40243a801d1e370c14f7fa36a71cab5cce7c1c035e9551fc7a0592901f3ea53";
