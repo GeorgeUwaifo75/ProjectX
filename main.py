fdd39d7463f7fe5127d3ae147d9c9324fbde2855
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-12-03 15:13:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 load_dotenv()
 
-urls = ["https://decrypt.co/news", "https://www.coindesk.com/", "https://thecryptobasic.com/","https://cryptopotato.com/"]
+urls = ["https://decrypt.co/news", "https://www.coindesk.com/", "https://thecryptobasic.com/","https://cryptopotato.com/","https://u.today/"]
 #arrx = ["BTC","Eth", "Sol"]
 
 def return_url(coin, COIN_API):
